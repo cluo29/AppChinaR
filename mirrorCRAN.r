98969a124db1a1a8f3beb44d7966f3785d030610
Chu Luo <mr.cluo29@gmail.com>
2016-11-19 18:02:00 +0200

    1
    1

---
+++ mirrorCRAN.r
@@ -0,0 +1,15 @@
+#https://mirrors.tuna.tsinghua.edu.cn/CRAN/
+#https://cran.r-project.org/mirrors.html
+
+# install.packages("RWeka")
+# 
+# install.packages("parallelSVM")
+# 
+# install.packages("e1071")
+
+#install.packages("randomForest")
+#install.packages("ROCR")
+
+#install.packages("RSQLite")
+#library(RSQLite)
+#library(ggplot2)
